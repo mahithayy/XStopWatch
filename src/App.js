fcaf31eb46a1c9ff0a926c725a6cb06c6ffc4401
mahithayy <hollamahitha@gmail.com>
2025-10-30 18:50:34 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import { useState, useEffect, useRef } from "react";
 
-function App() {
+export default function App() {
+  const [timer, setTimer] = useState(0);
+  const [isTimerStarted, setIsTimerStarted] = useState(false);
+  const timerId = useRef(null);
+
+  const handleStartStop = () => {
+    setIsTimerStarted(!isTimerStarted);
+  };
+
+  const handleReset = () => {
+    setTimer(0);
+    setIsTimerStarted(false);
+  };
+
+  useEffect(() => {
+    if (isTimerStarted) {
+      timerId.current = setTimeout(() => {
+        setTimer(timer + 1);
+      }, 1000);
+    }
+    return () => clearTimeout(timerId.current);
+  }, [timer, isTimerStarted]);
+
+  const formatTime = (timer) => {
+    const mins = Math.floor(timer / 60);
+    const secs = timer % 60;
+    return `${mins}:${secs < 10 ? "0" : ""}${secs}`;
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{formatTime(timer)}</h1>
+      <button onClick={handleStartStop}>
+        {isTimerStarted ? "stop" : "start"}
+      </button>
+      <button onClick={handleReset}>Reset</button>
     </div>
   );
 }
-
-export default App;
