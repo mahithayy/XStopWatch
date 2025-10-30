75374b4cb8082d842eb9732894320ad8686f754a
mahithayy <hollamahitha@gmail.com>
2025-10-30 19:03:29 +0530

    test compatability changes

--- src/App.js
+++ src/App.js
@@ -3,37 +3,40 @@ import { useState, useEffect, useRef } from "react";
 
 export default function App() {
   const [timer, setTimer] = useState(0);
-  const [isTimerStarted, setIsTimerStarted] = useState(false);
+  const [isRunning, setIsRunning] = useState(false);
   const timerId = useRef(null);
 
   const handleStartStop = () => {
-    setIsTimerStarted(!isTimerStarted);
+    setIsRunning(!isRunning);
   };
 
   const handleReset = () => {
+    clearTimeout(timerId.current);
     setTimer(0);
-    setIsTimerStarted(false);
+    setIsRunning(false);
   };
 
   useEffect(() => {
-    if (isTimerStarted) {
+    if (isRunning) {
       timerId.current = setTimeout(() => {
-        setTimer(timer + 1);
+        setTimer((prev) => prev + 1);
       }, 1000);
     }
     return () => clearTimeout(timerId.current);
-  }, [timer, isTimerStarted]);
+  }, [isRunning, timer]);
 
-  const formatTime = (timer) => {
-    const mins = Math.floor(timer / 60);
-    const secs = timer % 60;
+  const formatTime = (time) => {
+    const mins = Math.floor(time / 60);
+    const secs = time % 60;
     return `${mins}:${secs < 10 ? "0" : ""}${secs}`;
   };
+
   return (
     <div className="App">
-      <h1>{formatTime(timer)}</h1>
+      <h1>Stopwatch</h1>
+      <h2>{formatTime(timer)}</h2>
       <button onClick={handleStartStop}>
-        {isTimerStarted ? "stop" : "start"}
+        {isRunning ? "Stop" : "Start"}
       </button>
       <button onClick={handleReset}>Reset</button>
     </div>
